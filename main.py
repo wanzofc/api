84ecd727d31e0664d47692c225c7742880a4353d
Melodic-XT <raflyphising@gmail.com>
2021-01-03 15:36:48 +0000

    make it better

--- main.py
+++ main.py
@@ -1031,7 +1031,7 @@ def zotaku():
             query = request.args.get('q')
             data = []
             url = bsoup("https://otakudesu.tv/?s={}&post_type=anime".format(query))
-            for rafly in url.find_all('div',class_='item episode-home'):
+            for rafly in url.find_all('div',class_='venser'):
                 image = "{}".format(str(rafly.find('img')['src']))
                 link = "{}".format(str(rafly.find('a')['href']))
                 data = bsoup(link)
