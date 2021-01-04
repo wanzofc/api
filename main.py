fd8ad5bd5a64282dc12257898004beeaee71f88f
Melodic-XT <raflyphising@gmail.com>
2021-01-04 10:54:51 +0000

    make it better

--- main.py
+++ main.py
@@ -1041,24 +1041,10 @@ def zkomik():
                 img = tobz.img['src']
                 image = shorturl(img)
                 link = tobz.a['href']
-                soup = bsoup(link)
-                info = soup.find("div",class_="infox")
-                txt = info.findAll('span')
-                status = txt[0].text.replace('Status: ','')
-                format = txt[1].text.replace('Format: ','')
-                rilis = txt[2].text.replace('Dirilis: ','')
-                pengarang = txt[3].text.replace('Pengarang: ','')
-                jenis = txt[4].text.replace('Jenis Komik: ','')
-                umur = txt[5].text.replace('Umur Pembaca: ','')
-                cara = txt[6].text.replace('Cara Baca ','')
-                konsep = txt[7].text.replace('Konsep Cerita: ','')
-                update = txt[8].text.replace('Update Terakhir: ','')
-                dilihat = txt[9].text.replace('Dilihat: ','')
-                genres = soup.find('div', class_='genre-info').text.replace('\n',', ')
                 rat = tobz.find('div', class_='rating')
                 rate = rat.findAll('i')
                 rating = rate[0].text
-                hasil = hasilnya.append({"judul":title,"thumbnail":image,"rating":rating,"link":link,"status":status,"format":format,"dirilis":rilis,"pengarang":pengarang,"jenis_komik":jenis,"umur_pembaca":umur,"cara_baca":cara,"konsep_cerita":konsep,"update_terakhir":update,"genre":genres})
+                hasil = hasilnya.append({"judul":title,"thumbnail":image,"rating":rating,"link":link})
             return {
 				'status': 200,
 				'creator':'Tobz',
