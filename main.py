94b4cac6c84bad4ac528b99336129e93f6b55754
Melodic-XT <raflyphising@gmail.com>
2021-01-05 16:31:31 +0000

    make it better

--- main.py
+++ main.py
@@ -1239,12 +1239,7 @@ def zneonime():
                 link = "{}".format(str(Tobz.find('a')['href']))
                 title = "{}".format(str(Tobz.find('img')['alt']))
                 image = "{}".format(str(Tobz.find('img')['data-src'])).replace(' ',"")
-                data = bsoup(link)
-                data2 = data.find('div', class_='meta-b')
-                info2 = data2.findAll('span', class_='metx')
-                season = info2[0].text.replace(' Season ','')
-                episode = info2[1].text.replace(' Episode ','')
-                hasil = hasilnya.append({"title":title,"desc": desc,"image":image,"link":link,'season':season,'episode':episode})
+                hasil = hasilnya.append({"title":title,"desc": desc,"image":image,"link":link})
             return {
 				'status': 200,
 				'creator':'Tobz',
