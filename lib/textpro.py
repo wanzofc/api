9d045ef1a8a90222ae57322e0a933033e514252b
Melodic-XT <raflyphising@gmail.com>
2021-01-07 08:37:43 +0000

    make it better

--- lib/textpro.py
+++ lib/textpro.py
@@ -17,7 +17,25 @@ class tp:
             'wolflogo2': '/create-wolf-logo-galaxy-online-936.html',
             'blood': '/blood-text-on-the-frosted-glass-941.html',
             'dropwater': '/dropwater-text-effect-872.html',
-            'snow': '/create-snow-text-effects-for-winter-holidays-1005.html'
+            'snow': '/create-snow-text-effects-for-winter-holidays-1005.html',
+            }
+
+    def snow(self, text):
+        '''
+        text = rainbow
+        '''
+        try:
+            url = self.BaseUrl.format(self.theme['snow'])
+            token = bs(r.get(url).text, 'html.parser').find('input', id='token')['value']
+            data = {u'text[]': [u'%s' % text], 'submit': 'Go', 'token': token}
+            result = self.BaseUrl.format(bs(r.post(url, data).text, 'html.parser').find('div', class_='btn-group').a['href'])
+            return {
+                'result': result
+            }
+        except Exception as e:
+            print(e)
+            return {
+                'error': 'ERROR'
             }
 
     def neon_light(self, text):
@@ -26,6 +44,24 @@ class tp:
         '''
         try:
             url = self.BaseUrl.format(self.theme['neon_light'])
+            token = bs(r.get(url).text, 'html.parser').find('input', id='token')['value']
+            data = {u'text[]': [u'%s' % text], 'submit': 'Go', 'token': token}
+            result = self.BaseUrl.format(bs(r.post(url, data).text, 'html.parser').find('div', class_='btn-group').a['href'])
+            return {
+                'result': result
+            }
+        except Exception as e:
+            print(e)
+            return {
+                'error': 'ERROR'
+            }
+
+    def neon_technology(self, text):
+        '''
+        text = rainbow
+        '''
+        try:
+            url = self.BaseUrl.format(self.theme['neon_technology'])
             token = bs(r.get(url).text, 'html.parser').find('input', id='token')['value']
             data = {u'text[]': [u'%s' % text], 'submit': 'Go', 'token': token}
             result = self.BaseUrl.format(bs(r.post(url, data).text, 'html.parser').find('div', class_='btn-group').a['href'])
