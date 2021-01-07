69fb0976374164487e11c0f44c62bbeb13de501a
Melodic-XT <raflyphising@gmail.com>
2021-01-07 08:20:30 +0000

    make it better

--- lib/textpro.py
+++ lib/textpro.py
@@ -20,48 +20,12 @@ class tp:
             'snow': '/create-snow-text-effects-for-winter-holidays-1005.html'
             }
 
-    def snow(self, text):
-        '''
-        text = rainbow
-        '''
-        try:
-            url = self.BaseUrl.format(self.theme['snow'])
-            token = bs(r.get(url).text, 'html.parser').find('input', id='token')['value']
-            data = {u'text[]': [u'%s' % text], 'submit': 'Go', 'token': token}
-            result = self.BaseUrl.format(bs(r.post(url, data).text, 'html.parser').find('div', class_='btn-group').a['href'])
-            return {
-                'result': result
-            }
-        except Exception as e:
-            print(e)
-            return {
-                'error': 'ERROR'
-            }
-
     def neon_light(self, text):
         '''
         text = rainbow
         '''
         try:
             url = self.BaseUrl.format(self.theme['neon_light'])
-            token = bs(r.get(url).text, 'html.parser').find('input', id='token')['value']
-            data = {u'text[]': [u'%s' % text], 'submit': 'Go', 'token': token}
-            result = self.BaseUrl.format(bs(r.post(url, data).text, 'html.parser').find('div', class_='btn-group').a['href'])
-            return {
-                'result': result
-            }
-        except Exception as e:
-            print(e)
-            return {
-                'error': 'ERROR'
-            }
-
-    def neon_technology(self, text):
-        '''
-        text = rainbow
-        '''
-        try:
-            url = self.BaseUrl.format(self.theme['neon_technology'])
             token = bs(r.get(url).text, 'html.parser').find('input', id='token')['value']
             data = {u'text[]': [u'%s' % text], 'submit': 'Go', 'token': token}
             result = self.BaseUrl.format(bs(r.post(url, data).text, 'html.parser').find('div', class_='btn-group').a['href'])
